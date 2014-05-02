6684b6614a2c08281ca58e499c4770ab64be4904
Stathis Sideris <stathis@pixelated-noise.com>
2014-05-02 12:13:34 +0100

    Add F1 navigation for no-window emacs

--- .emacs.d/no-window.el
+++ .emacs.d/no-window.el
@@ -2,4 +2,17 @@
 
 (menu-bar-mode -1)
 
+(global-unset-key (kbd "<f1>"))
+
+(global-set-key (kbd "<f1> <left>")  'windmove-left)
+(global-set-key (kbd "<f1> <right>") 'windmove-right)
+(global-set-key (kbd "<f1> <up>")    'windmove-up)
+(global-set-key (kbd "<f1> <down>")  'windmove-down)
+
+(global-set-key (kbd "<f1> <f1>")  'mark-sexp)
+
+(global-set-key (kbd "<f1> .") 'highlight-symbol-next)
+(global-set-key (kbd "<f1> ,") 'highlight-symbol-prev)
+
+
 (provide 'no-window)
