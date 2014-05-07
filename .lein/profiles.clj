9cf5866555d54c45c7f1c5e3d65c2d40c7fedd09
Stathis Sideris <stathis@pixelated-noise.com>
2014-05-07 13:04:53 +0000

    Added cider-nrepl middleware

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -11,4 +11,5 @@
                         [alex-and-georges.debug-repl debug-repl]
                         [com.georgejahad.difform difform]])]
         :plugins [[lein-pprint "1.1.1"]
-                  [quickie "0.2.5"]]}}
+                  [quickie "0.2.5"]
+                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}}
