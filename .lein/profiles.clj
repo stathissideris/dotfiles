5878f68fd1f61872aa0e2b9df6b1274b32a2c8fe
Stathis Sideris <stathis@pixelated-noise.com>
2015-04-21 22:18:38 +0000

    Updating profiles.clj again

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,18 +1,18 @@
 {:user {:signing {:gpg-key "25C79D92"}
-        :dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
-                       [im.chit/vinyasa "0.2.1"
+        :dependencies [#_[org.clojars.gjahad/debug-repl "0.3.3"]
+                       #_[im.chit/vinyasa "0.2.1"
                         :exclusions [org.apache.httpcomponents/httpclient
                                      org.apache.httpcomponents/httpcore]]
                        [difform "1.1.2"]
                        [pjstadig/humane-test-output "0.6.0"]
                        [org.clojure/tools.nrepl "0.2.7"]]
-        :injections [(require 'vinyasa.inject)
-                     (require 'vinyasa.pull)
-                     (require 'alex-and-georges.debug-repl)
-                     (require 'com.georgejahad.difform)
+        :injections [#_(require 'vinyasa.inject)
+                     #_(require 'vinyasa.pull)
+                     #_(require 'alex-and-georges.debug-repl)
+                     #_(require 'com.georgejahad.difform)
                      (require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)
-                     (vinyasa.inject/inject
+                     #_(vinyasa.inject/inject
                       'clojure.core
                       '[[vinyasa.pull pull]
                         [alex-and-georges.debug-repl debug-repl]
@@ -21,4 +21,5 @@
                        :notify-on-success true}
         :plugins [[lein-pprint "1.1.2"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [refactor-nrepl "1.0.3"]
                   [com.jakemccrary/lein-test-refresh "0.5.4"]]}}
