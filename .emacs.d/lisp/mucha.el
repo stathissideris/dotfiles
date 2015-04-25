0dbfd4df64a08b9ad7adacefddf36133579a54b9
Stathis Sideris <stathis@pixelated-noise.com>
2015-04-25 04:26:42 +0100

    Open files from dired in Windows

--- .emacs.d/lisp/mucha.el
+++ .emacs.d/lisp/mucha.el
@@ -8,4 +8,14 @@
 (setq magit-git-executable "c:\\Program Files (x86)\\Git\\bin\\git.exe")
 (setq magit-process-quote-curly-braces nil)
 
+(defun w32-dired-open-explorer ()
+  "Open a file in dired mode by explorer.exe as you double click it."
+  (interactive)
+  (let ((file-name (dired-get-file-for-visit)))
+    (if (file-exists-p file-name)
+	(w32-shell-execute "open" file-name nil 1))))
+
+;; Bind it to `o' in dired mode.
+(define-key dired-mode-map "o" 'w32-dired-open-explorer)
+
 (provide 'mucha)
