ae746add493be707e67dcfeae9ba5ededf1099ca
Stathis Sideris <stathis@pixelated-noise.com>
2015-10-20 02:49:55 +0300

    Re-introduce vinyasa

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,25 +1,24 @@
-{:user {:signing {:gpg-key "7AF00D02"}
-        :dependencies [#_[org.clojars.gjahad/debug-repl "0.3.3"]
-                       #_[im.chit/vinyasa "0.2.1"
-                        :exclusions [org.apache.httpcomponents/httpclient
-                                     org.apache.httpcomponents/httpcore]]
-                       [difform "1.1.2"]
-                       [pjstadig/humane-test-output "0.7.0"]
-                       [org.clojure/tools.nrepl "0.2.11"]]
-        :injections [#_(require 'vinyasa.inject)
-                     #_(require 'vinyasa.pull)
-                     #_(require 'alex-and-georges.debug-repl)
-                     #_(require 'com.georgejahad.difform)
-                     (require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)
-                     #_(vinyasa.inject/inject
-                      'clojure.core
-                      '[[vinyasa.pull pull]
-                        [alex-and-georges.debug-repl debug-repl]
-                        [com.georgejahad.difform difform]])]
-        :test-refresh {:notify-command ["tmux" "display-message"]
-                       :notify-on-success true}
-        :plugins [[lein-pprint "1.1.2"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [com.jakemccrary/lein-test-refresh "0.5.4"]]}}
+{:user
+ {:signing {:gpg-key "7AF00D02"}
+  :dependencies [#_[org.clojars.gjahad/debug-repl "0.3.3"]
+                 #_[difform "1.1.2"]
+                 [alembic "0.3.2"]
+                 [im.chit/vinyasa "0.4.2"]
+                 [pjstadig/humane-test-output "0.7.0"]
+                 [org.clojure/tools.nrepl "0.2.11"]]
+  :injections [(require '[vinyasa.inject :as inject])
+               #_(require 'alex-and-georges.debug-repl)
+               #_(require 'com.georgejahad.difform)
+               (require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)
+               (vinyasa.inject/in
+                clojure.core
+                [alembic.still [distill pull]]
+                [clojure.pprint pprint]
+                [clojure.repl doc source pst])]
+  :test-refresh {:notify-command ["tmux" "display-message"]
+                 :notify-on-success true}
+  :plugins [[lein-pprint "1.1.2"]
+            [refactor-nrepl "2.0.0-SNAPSHOT"]
+            [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+            [com.jakemccrary/lein-test-refresh "0.5.4"]]}}
