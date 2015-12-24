c2feef268a1c30fe4bb2a1ae0800d704322b218f
Stathis Sideris <stathis@juxt.pro>
2015-12-24 22:30:27 +0000

    emacs: ttl-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -868,6 +868,9 @@ by using nxml's indentation rules."
 (require 'tiling)
 (define-key global-map (kbd "C-\\") 'tiling-cycle)
 
+(require 'ttl-mode)
+
+
 ;; Flash active buffer
 
 (global-set-key (kbd "C-c ;") 'flash-active-buffer)
