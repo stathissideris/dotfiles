bf13ce9e00d1caf576bf19caa427f4c42da96b94
Stathis Sideris <stathis@juxt.pro>
2016-01-30 10:51:14 +0000

    Add hyper key

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -214,11 +214,11 @@
 ;; setting the PC keyboard's various keys to
 ;; Super or Hyper, for emacs running on Windows.
 
-(setq w32-pass-lwindow-to-system nil 
-      w32-pass-rwindow-to-system nil 
-      w32-pass-apps-to-system nil 
-      w32-lwindow-modifier 'super ; Left Windows key 
-      w32-rwindow-modifier 'super ; Right Windows key 
+(setq w32-pass-lwindow-to-system nil
+      w32-pass-rwindow-to-system nil
+      w32-pass-apps-to-system nil
+      w32-lwindow-modifier 'super ; Left Windows key
+      w32-rwindow-modifier 'super ; Right Windows key
       w32-apps-modifier 'hyper) ; Menu key
 
 ;; navigation
