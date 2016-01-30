513c279415b51b90a70a59e8bf00539a4ef1adf4
Stathis Sideris <stathis@juxt.pro>
2016-01-30 10:50:15 +0000

    Enable clj-refactor

--- .emacs.d/lisp/clojure-dev.el
+++ .emacs.d/lisp/clojure-dev.el
@@ -1,5 +1,5 @@
 (require 'inf-lisp)
-;;(require 'clj-refactor)
+(require 'clj-refactor)
 (require 'align-cljlet)
 (require 'cider-macroexpansion)
 
@@ -50,9 +50,10 @@
   (paredit-mode 1)
   (highlight-symbol-mode 1)
 
-  ;;(clj-refactor-mode 1)
+  (clj-refactor-mode 1)
   (yas-minor-mode 1)
-  (cljr-add-keybindings-with-prefix "C-c C-v"))
+  (cljr-add-keybindings-with-prefix "C-c C-v")
+  )
 
 (add-hook 'clojure-mode-hook 'clojure-hook)
 (define-key clojure-mode-map (kbd "C-c C-a") 'align-cljlet)
