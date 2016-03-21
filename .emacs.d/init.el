805cb921c35ac119bb01ad8f058cc83823adc02d
Stathis Sideris <stathis@pixelated-noise.com>
2016-03-21 20:46:07 +0200

    emacs: fix multiple cursors key bindings to work with projectile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -791,10 +791,10 @@ by using nxml's indentation rules."
 (browse-kill-ring-default-keybindings)
 
 (maybe-install-and-require 'multiple-cursors)
-(global-set-key (kbd "C-c .") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-c ,") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c /") 'mc/mark-all-dwim)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C-x .") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-x ,") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-x /") 'mc/mark-all-dwim)
+(global-set-key (kbd "C-S-x C-S-x") 'mc/edit-lines)
 
 (defun tabs->newlines ()
   (interactive)
