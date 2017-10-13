03d6978fc4b13063df6fce8de64448ba7f6f9a0a
Stathis Sideris <stathis@pixelated-noise.com>
2017-10-13 13:41:23 +0300

    update leiningen stuff

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {:signing {:gpg-key "67D85DDC"}
-  :dependencies [[alembic "0.3.2"]
-                 [org.clojure/tools.nrepl "0.2.12"]
+  :dependencies [;;[alembic "0.3.2"]
+                 ;;[org.clojure/tools.nrepl "0.2.12"]
                  ;;[im.chit/vinyasa "0.4.7"]
                  [pjstadig/humane-test-output "0.8.0"]]
   :injections [#_(require '[vinyasa.inject :as inject])
@@ -14,17 +14,19 @@
                 [clojure.repl doc source pst]
                 [clojure.data diff])
                ;;don't work:
-               (defn >ppr [x] (clojure.pprint/pprint x) x)
-               (defn >show-diff [a b]
+               #_(defn >ppr [x] (clojure.pprint/pprint x) x)
+               #_(defn >show-diff [a b]
                  (let [[removed added] (clojure.data/diff a b)]
                    (println "Removed:")
                    (clojure.pprint/pprint removed)
                    (println "\nAdded:")
                    (clojure.pprint/pprint added)))]
   :test-refresh {;;:notify-command ["tmux" "display-message"]
-                 :notify-on-success true}
-  :plugins [[cider/cider-nrepl "0.14.0"]
-            [lein-pprint "1.1.2"]
-            [refactor-nrepl "2.2.0"]
-            [lein-ancient "0.6.10"]
+                 ;;:notify-on-success true
+                 }
+  :plugins [[cider/cider-nrepl "0.15.1"]
+            ;;[lein-pprint "1.1.2"]
+            ;;[refactor-nrepl "2.2.0"]
+            ;;[lein-ancient "0.6.10"]
+            [lein-ns-dep-graph "0.2.0-SNAPSHOT"] ;;run with: lein ns-dep-graph
             [com.jakemccrary/lein-test-refresh "0.16.0"]]}}
