e335ce22410948319958eda18aeb9225d01b89ff
Stathis Sideris <stathis@pixelated-noise.com>
2020-10-07 20:24:34 +0300

    Fixes for windows

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -561,7 +561,7 @@
         org-table-convert-region-max-lines 999)
   (org-babel-do-load-languages 'org-babel-load-languages
                                '((shell      . t)
-                                 (http       . t)
+                                 ;;(http       . t)
                                  (js         . t)
                                  (emacs-lisp . t)
                                  (perl       . t)
@@ -921,68 +921,6 @@
   (defun ss/python-kill-buffer ()
     (interactive)
     (kill-buffer (buffer-name (current-buffer)))))
-
-(use-package wl
-  :init
-  (autoload 'wl "wl" "Wanderlust" t)
-  (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
-  (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
-
-  ;; IMAP
-  (setq elmo-imap4-default-server "imap.gmail.com")
-  (setq elmo-imap4-default-user "sideris@gmail.com")
-  (setq elmo-imap4-default-authenticate-type 'clear)
-  (setq elmo-imap4-default-port '993)
-  (setq elmo-imap4-default-stream-type 'ssl)
-
-  (setq elmo-imap4-use-modified-utf7 t)
-
-  ;; SMTP
-  (setq wl-smtp-connection-type 'starttls)
-  (setq wl-smtp-posting-port 587)
-  (setq wl-smtp-authenticate-type "plain")
-  (setq wl-smtp-posting-user "sideris")
-  (setq wl-smtp-posting-server "smtp.gmail.com")
-  (setq wl-local-domain "gmail.com")
-
-  (setq wl-default-folder "%inbox")
-  (setq wl-default-spec "%")
-  (setq wl-draft-folder "%[Gmail]/Drafts") ; Gmail IMAP
-  (setq wl-trash-folder "%[Gmail]/Trash")
-
-  (setq wl-folder-check-async t)
-
-  (setq elmo-imap4-use-modified-utf7 t)
-
-  (setq
-   wl-message-ignored-field-list '("^.*:")
-   wl-message-visible-field-list
-   '("^\\(To\\|Cc\\):"
-     "^Subject:"
-     "^\\(From\\|Reply-To\\):"
-     "^Organization:"
-     "^Message-Id:"
-     "^\\(Posted\\|Date\\):"
-     )
-   wl-message-sort-field-list
-   '("^From"
-     "^Organization:"
-     "^X-Attribution:"
-     "^Subject"
-     "^Date"
-     "^To"
-     "^Cc"))
-
-  (autoload 'wl-user-agent-compose "wl-draft" nil t)
-  (if (boundp 'mail-user-agent)
-      (setq mail-user-agent 'wl-user-agent))
-  (if (fboundp 'define-mail-user-agent)
-      (define-mail-user-agent
-        'wl-user-agent
-        'wl-user-agent-compose
-        'wl-draft-send
-        'wl-draft-kill
-        'mail-send-hook)))
 
 ;; (setq python-shell-interpreter "python"
 ;;       python-shell-interpreter-args "-i"
@@ -1614,7 +1552,7 @@
 (setq mouse-wheel-scroll-amount '(1)) ; Distance in pixel-resolution to scroll each mouse wheel event.
 (setq mouse-wheel-progressive-speed nil)
 
-(require 'flycheck-joker)
+;;(require 'flycheck-joker)
 
 (defun ss/sql-format (beg end)
   "Beautify SQL in region between beg and END.
