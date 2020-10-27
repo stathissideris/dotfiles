08a456cc2ab2e98cf645a4606cc16db619eee1a9
Stathis Sideris <stathis@pixelated-noise.com>
2020-10-27 15:24:28 +0200

    windows tweaks

--- .emacs.d/lisp/mucha.el
+++ .emacs.d/lisp/mucha.el
@@ -2,11 +2,16 @@
 
 (global-set-key [f8] `(lambda () (interactive) (find-file "s:/global/notes/notes.org")))
 
+(setq default-directory "~")
+
 ;;for debugging:
 (setq mucha-options-loaded t)
 
 (setq magit-git-executable "c:\\Program Files\\Git\\bin\\git.exe")
 (setq magit-process-quote-curly-braces nil)
+(global-magit-file-mode -1)
+(setq magit-auto-revert-mode nil)
+
 
 (defun w32-dired-open-explorer ()
   "Open a file in dired mode by explorer.exe as you double click it."
@@ -19,7 +24,7 @@
 (define-key dired-mode-map "o" 'w32-dired-open-explorer)
 
 (set-face-attribute 'default nil :family "Consolas")
-;;(set-face-attribute 'default nil :height 120)
+(set-face-attribute 'default nil :height 105)
 (set-face-attribute 'fixed-pitch nil :family "Consolas")
 
 (provide 'mucha)
